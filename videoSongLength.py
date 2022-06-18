436d71d3fc44e95c3670dae12affe63dfb3ecb6a
d-muhr <105784542+d-muhr@users.noreply.github.com>
2022-06-18 18:42:49 +0200

    use autopep8 tool

--- videoSongLength.py
+++ videoSongLength.py
@@ -16,55 +16,63 @@ from datetime import timedelta
 import pyinputplus as pyip
 import sys
 
-#The welcome text is displayed.
+# The welcome text is displayed.
 print('''FOR HOW LONG A VIDEO OR SONG HAS BEEN PLAYED
 Many videos and songs have been played millions of times. But for how long has each been played (e.g. weeks/months/years) if each time counts?
 This calculator enables you to find it out!
 ''')
 
 while True:
-    #The user gives information about the song/video.
-    input_number_played = pyip.inputInt(min = 1, max = 9999999999999999, prompt = 'How many times has the video or song been played? Please type the number (e.g. "1000") and press Enter >>> ')
+    # The user gives information about the song/video.
+    input_number_played = pyip.inputInt(
+        min=1,
+        max=9999999999999999,
+        prompt='How many times has the video or song been played? Please type the number (e.g. "1000") and press Enter >>> ')
 
     print("How long is the video or song? Please type the number of hours, minutes and seconds.")
 
-    input_hours = pyip.inputInt(min = 0, max = 100, prompt = 'number of hours: >>> ' )
-    input_minutes = pyip.inputInt(min = 0, max = 59, prompt= 'number of minutes: >>>' )
-    input_seconds = pyip.inputInt(min = 0, max = 59, prompt = 'number of seconds: >>> ')
-    
+    input_hours = pyip.inputInt(min=0, max=100, prompt='number of hours: >>> ')
+    input_minutes = pyip.inputInt(
+        min=0, max=59, prompt='number of minutes: >>>')
+    input_seconds = pyip.inputInt(
+        min=0, max=59, prompt='number of seconds: >>> ')
+
     '''
     The amount of hours, minutes and seconds of the song/video being played is calculated.
     '''
-    time_input_delta = timedelta(hours = input_hours) + timedelta(minutes= input_minutes) + timedelta(seconds= input_seconds)
-    time_input_muliplied_delta = time_input_delta *  input_number_played
+    time_input_delta = timedelta(
+        hours=input_hours) + timedelta(minutes=input_minutes) + timedelta(seconds=input_seconds)
+    time_input_muliplied_delta = time_input_delta * input_number_played
 
     # (((TODO: I might change the folowing 6 variables with the
     # ~symbol change feature into "output_in_seconds" etc. as this is
     # probably a clearer name)))
-    time_in_seconds = time_input_muliplied_delta / timedelta(seconds = 1)
-    time_in_minutes = time_input_muliplied_delta / timedelta(minutes = 1)
-    time_in_hours = time_input_muliplied_delta / timedelta(hours = 1)
-    time_in_days = time_input_muliplied_delta / timedelta(days = 1)
-    time_in_weeks = time_input_muliplied_delta / timedelta(weeks = 1)
-    time_in_years = time_input_muliplied_delta / timedelta(days = 365)
+    time_in_seconds = time_input_muliplied_delta / timedelta(seconds=1)
+    time_in_minutes = time_input_muliplied_delta / timedelta(minutes=1)
+    time_in_hours = time_input_muliplied_delta / timedelta(hours=1)
+    time_in_days = time_input_muliplied_delta / timedelta(days=1)
+    time_in_weeks = time_input_muliplied_delta / timedelta(weeks=1)
+    time_in_years = time_input_muliplied_delta / timedelta(days=365)
 
-    #TODO: For the seconds below it is not really necessary to make use of the "round"-funtion.
-    #The full amount of time the song/video has been played is displayed for the user.
-    print("---This video or song has been played for", round(time_in_years, 2), "years or", round(time_in_weeks, 2), "weeks or", round(time_in_days, 2), "days or", round(time_in_hours, 2), "hours or", round(time_in_minutes, 2), "minutes or", round(time_in_seconds, 2), "seconds.---")
-    
-    #The user can make another calculation or quit the program.
+    # TODO: For the seconds below it is not really necessary to make use of the "round"-funtion.
+    # The full amount of time the song/video has been played is displayed for
+    # the user.
+    print(
+        "---This video or song has been played for", round(
+            time_in_years, 2), "years or", round(
+            time_in_weeks, 2), "weeks or", round(
+                time_in_days, 2), "days or", round(
+                    time_in_hours, 2), "hours or", round(
+                        time_in_minutes, 2), "minutes or", round(
+                            time_in_seconds, 2), "seconds.---")
+
+    # The user can make another calculation or quit the program.
     print("Do you want to make another calculation or quit the program?")
-    proceed_how = pyip.inputMenu(['AGAIN', 'QUIT'], lettered=True, numbered=False)
-    
+    proceed_how = pyip.inputMenu(
+        ['AGAIN', 'QUIT'], lettered=True, numbered=False)
+
     if proceed_how == 'AGAIN':
         continue
     if proceed_how == 'QUIT':
         print("Thanks for using the program!")
         sys.exit()
-
-
-
-
-
-
-
